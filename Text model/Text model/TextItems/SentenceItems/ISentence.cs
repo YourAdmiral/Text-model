edc13ddf0e5d04b67021ee6791be3ded54d8c4d1
MANDOM <yourmarshall@yandex.by>
2020-12-12 21:32:31 +0300

    Added ISentence, Sentence
    Sentence - represent unit of text.

---
+++ Text model/Text model/TextItems/SentenceItems/ISentence.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_model
+{
+    internal interface ISentence : IEnumerable<ISentenceItem>
+    {
+        public void Add(ISentenceItem item);
+        public bool Remove(ISentenceItem item);
+        public int Count { get; }
+    }
+}
---
+++ Text model/Text model/TextItems/SentenceItems/Sentence.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Linq;
+
+namespace Text_model
+{
+    internal class Sentence : ISentence
+    {
+        private ICollection<ISentenceItem> _items;
+        public Sentence()
+        {
+            _items = new List<ISentenceItem>();
+        }
+        public Sentence(ICollection<ISentenceItem> source)
+        {
+            _items = source;
+        }
+        public void Add(ISentenceItem item)
+        {
+            if (item != null)
+                _items.Add(item);
+            else
+                throw new NullReferenceException("");
+        }
+        public bool Remove(ISentenceItem item)
+        {
+            if (item != null && _items.Contains(item))
+            {
+                _items.Remove(item);
+                return true;
+            }
+            return false;
+        }
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+        public IEnumerator<ISentenceItem> GetEnumerator()
+        {
+            return _items.AsEnumerable().GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+    }
+}
