02da9ae14e4c927dabf787fe2d0f53d32d42f3b3
MANDOM <yourmarshall@yandex.by>
2020-12-13 15:55:08 +0300

    Created factories

---
+++ Text model/Text model/Factories/PunctuationFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_model
+{
+    internal class PunctuationFactory : ISentenceItemFactory
+    {
+        public ISentenceItem Create(string chars)
+        {
+            return new Punctuation(chars);
+        }
+    }
+}
---
+++ Text model/Text model/Factories/WordFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_model
+{
+    internal class WordFactory : ISentenceItemFactory
+    {
+        public ISentenceItem Create(string chars)
+        {
+            return new Word(chars);
+        }
+    }
+}
