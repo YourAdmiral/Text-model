3ebbd02c6d718fccf01f6ef54100337f29f98b24
MANDOM <yourmarshall@yandex.by>
2020-12-13 18:54:24 +0300

    Added Separator

---
+++ Text model/Text model/TextManager/Separator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Text_model
+{
+    internal class Separator
+    {
+        private string[] _sentenceSeparators = new string[] { "?", "!", ".", "...", "?!", "!?" };
+        private string[] _wordSeparators = new string[] { " ", " - " };
+        private string[] _allSeparators = new string[] { 
+            "?", "!", ".", "...", "?!", "!?", ",", ":", ";", "'", " ", " - ",
+            "(", ")", "[", "]", "{", "}", "<", ">",  
+            "*", "/", "\"", "-", "+", ">=", "=<", "=", "==", "!=" 
+        };
+        public IEnumerable<string> SentenceSeparators()
+        {
+            return _sentenceSeparators.AsEnumerable();
+        }
+        public IEnumerable<string> WordSeparators()
+        {
+            return _wordSeparators.AsEnumerable();
+        }
+        public IEnumerable<string> All()
+        {
+            return _allSeparators.AsEnumerable();
+        }
+    }
+}
